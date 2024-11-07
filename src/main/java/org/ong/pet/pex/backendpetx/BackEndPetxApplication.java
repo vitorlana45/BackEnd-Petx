b29f76ee27b37cd3ce1d06b78b4c3fa8e71f8fe5
vitorlana45 <vitoresmerio00@gmail.com>
2024-11-07 15:24:37 -0300

    initial Commit

---
+++ src/main/java/org/ong/pet/pex/backendpetx/BackEndPetxApplication.java
@@ -0,0 +1,13 @@
+package org.ong.pet.pex.backendpetx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndPetxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndPetxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ong/pet/pex/backendpetx/BackEndPetxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ong.pet.pex.backendpetx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndPetxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
