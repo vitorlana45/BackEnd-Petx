24155d9bf22abe4c09c630c9f9e8fb70d8c3df14
vitorlana45 <vitoresmerio00@gmail.com>
2024-11-14 12:33:54 -0300

    feat: adicionado configuracao web

---
+++ src/main/java/org/ong/pet/pex/backendpetx/config/ConfiguracaoWeb.java
@@ -0,0 +1,18 @@
+package org.ong.pet.pex.backendpetx.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ConfiguracaoWeb implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
