b707d6ca6a3819b8f345990e0909b23e4f7bdc34
vitorlana45 <vitoresmerio00@gmail.com>
2024-11-27 12:32:59 -0300

    refactor: alterado de Post para Patch

--- src/main/java/org/ong/pet/pex/backendpetx/controllers/ProdutoController.java
+++ src/main/java/org/ong/pet/pex/backendpetx/controllers/ProdutoController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -51,7 +52,7 @@ public class ProdutoController {
     }
 
     @PreAuthorize("hasAnyRole('ADMIN', 'COLABORADOR')")
-    @PostMapping("/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<ProdutoDTOResposta> atualizarProduto(@PathVariable(value = "id") Long id, @RequestBody @Valid ProdutoDTO produtoAtualizado) {
         return ResponseEntity.ok(produtoService.atualizarProduto(id, produtoAtualizado));
     }
